19ea03c50a7d8b858c9a8489ecbfed280ad7a188
iamam305 <ajitesh314mishra@gmail.com>
2024-09-13 16:31:44 +0530

    rapidapi api key

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,26 @@ import "dotenv/config";
 import { swaggerUI } from "@hono/swagger-ui";
 import { download_file_from_url } from "./libs/download-file-from-url";
 import fs from "fs";
-import { logger } from 'hono/logger'
-import { compress } from 'hono/compress'
-import { cors } from 'hono/cors'
-
+import { logger } from "hono/logger";
+import { compress } from "hono/compress";
+import { cors } from "hono/cors";
+import { bearerAuth } from "hono/bearer-auth";
 const app = new OpenAPIHono();
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
 });
-app.use(logger())
-app.use(compress())
-app.use('/', cors())
-
+app.use(logger());
+app.use(compress());
+app.use("/", cors());
+app.use(
+  "/transcription",
+  bearerAuth({
+    headerName: "X-RapidAPI-Proxy-Secret",
+    token: process.env.RAPID_API_KEY!,
+    
+  })
+);
 
 const transcribe_route = createRoute({
   method: "post",
@@ -80,7 +87,6 @@ app.openapi(transcribe_route, async (c): Promise<any> => {
     );
   }
 
-
   if (translate) {
     console.log("Attempting to translate the file");
     const translation = await groq.audio.translations.create({
@@ -97,7 +103,7 @@ app.openapi(transcribe_route, async (c): Promise<any> => {
     });
   } else {
     console.log("Attempting to transcribe the file");
-   const transcription = await groq.audio.transcriptions.create({
+    const transcription = await groq.audio.transcriptions.create({
       file: fs.createReadStream(download_file_location),
       model: "whisper-large-v3",
       prompt: prompt,
@@ -111,8 +117,6 @@ app.openapi(transcribe_route, async (c): Promise<any> => {
       output: transcription,
     });
   }
-
-  
 });
 
 app.doc("/docs", {
@@ -123,7 +127,7 @@ app.doc("/docs", {
   },
 });
 
-app.get('/ping', async (c) => c.text('pong'))
+app.get("/ping", async (c) => c.text("pong"));
 app.get("/swagger-ui", swaggerUI({ url: "/docs" }));
 // export const handler = handle(app);
 const port = process.env.PORT || 5000;
